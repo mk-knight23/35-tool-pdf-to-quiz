1f02b67d13d482a046d3779976f04ff4105e4805
kazi-reprime <kazi@reprime.com>
2026-07-17 22:52:33 +0530

    fix: load GTM tag unconditionally in head as standard setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,8 +73,33 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             crossOrigin="anonymous"
           />
         )}
+        {/* Google Tag Manager */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+              `,
+            }}
+          />
+        )}
       </head>
       <body className="flex min-h-dvh flex-col bg-surface text-ink antialiased relative overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
         {/* Floating animated blobs */}
         <div className="qf-blob qf-blob-1" aria-hidden="true" />
         <div className="qf-blob qf-blob-2" aria-hidden="true" />
--- src/components/layout/AnalyticsScripts.tsx
+++ src/components/layout/AnalyticsScripts.tsx
@@ -17,7 +17,6 @@ import { CONSENT_EVENT } from "./ConsentBanner";
  * touches the page. It reacts to consent changes via the CONSENT_EVENT so the
  * user does not need to reload after accepting.
  */
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 const IS_PROD = process.env.NODE_ENV === "production";
 
@@ -40,16 +39,6 @@ export function AnalyticsScripts() {
 
   return (
     <>
-      {GTM_ID ? (
-        <Script id="gtm-loader" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}
-        </Script>
-      ) : null}
-
       {GA_ID ? (
         <>
           <Script
